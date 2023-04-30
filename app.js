768c7b3c00d2d6b692b03499c9fd1d85440e3d33
Ajamu Page <kil91402@gmail.com>
2023-04-30 17:16:30 -0500

    added hours

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 'use strict'
 
-function getRandomInt (min,max) {
+let hours = ['8AM', '9AM', '10AM', '11AM', '12AM'];
+
+function getRandomNumber (min,max) {
     min = Math.ceil(min);
     max = Math.floor(max);
-    return Math.floor(Math.random() * (max-min) + min);
+    return Math.floor(Math.random() * (max - min + 1) + min);
 }
+
+
+
 
 
 
