cf9afa8897e2be76a36dd97a63a94733bce7ce92
Ajamu Page <kil91402@gmail.com>
2023-05-04 21:20:49 -0500

    changes

--- js/expresso.js
+++ js/expresso.js
@@ -3,5 +3,10 @@
 let salesPrice = 5.99;
 let discount = 10;
 let salesTax = 5.2;
-let totalPrice = salesPrice - (salesPrice * discount/100);
+let quantity = prompt ('How many cups would you like?');
+let Price = salesPrice - (salesPrice * discount/100);
+let totalPrice= (Price * quantity);
 let finalPrice = totalPrice + (totalPrice * salesTax/100);
+
+
+alert(`Your final price is ${finalPrice}`);
