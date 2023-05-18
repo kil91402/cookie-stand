75bb343b2df9cc10fea4215d9f3fd96fb347cecc
Ajamu Akil Page <kil91402@gmail.com>
2023-05-18 01:02:46 +0000

    Push Changes

--- js/app.js
+++ js/app.js
@@ -1,15 +1,44 @@
 "use strict";
 
-let hours = ['8am', '9am', '10am', '11am', '12pm']
+let hours = ["8am", "9am", "10am", "11am", "12pm"];
 
 let storesArray = [
-     { store: "Seattle", minCust: 23, maxCust: 65, avgCookiesPerSale: 6.3, salesPerHour: [] },
-     { store: "Tokyo", minCust: 3, maxCust: 24, avgCookiesPerSale: 1.2, salesPerHour: [] },
-     { store: "Dubai", minCust: 11, maxCust: 38, avgCookiesPerSale: 3.7, salesPerHour: [] },
-     { store: "Paris", minCust: 20, maxCust: 38, avgCookiesPerSale: 2.3, salesPerHour: [] },
-     { store: "Lima", minCust: 2, maxCust: 16, avgCookiesPerSale: 4.6, salesPerHour: [] },
-    ];
-   
+  {
+    store: "Seattle",
+    minCust: 23,
+    maxCust: 65,
+    avgCookiesPerSale: 6.3,
+    salesPerHour: [],
+  },
+  {
+    store: "Tokyo",
+    minCust: 3,
+    maxCust: 24,
+    avgCookiesPerSale: 1.2,
+    salesPerHour: [],
+  },
+  {
+    store: "Dubai",
+    minCust: 11,
+    maxCust: 38,
+    avgCookiesPerSale: 3.7,
+    salesPerHour: [],
+  },
+  {
+    store: "Paris",
+    minCust: 20,
+    maxCust: 38,
+    avgCookiesPerSale: 2.3,
+    salesPerHour: [],
+  },
+  {
+    store: "Lima",
+    minCust: 2,
+    maxCust: 16,
+    avgCookiesPerSale: 4.6,
+    salesPerHour: [],
+  },
+];
 
 function getRandomInt(min, max) {
   min = Math.ceil(min);
@@ -17,26 +46,26 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
 }
 
-for (let store of storesArray) { 
+for (let store of storesArray) {
   console.log("store", store);
 
-
-  for (let hour of hours) { 
-
-    let randomInteger = getRandomInt(store["minCust"], store["maxCust"]); 
-    let salesAmount = randomInteger * store["avgCookiesPerSale"]; 
+  for (let hour of hours) {
+    let randomInteger = getRandomInt(store["minCust"], store["maxCust"]);
+    let salesAmount = randomInteger * store["avgCookiesPerSale"];
     store["salesPerHour"].push(salesAmount);
-      
-} 
+  }
 }
 console.log("storesArray", storesArray);
 
+let parent = document.getElementById("store");
+for (let store of storesArray) {
+  let headingElement = document.createElement("h2");
+  headingElement.textContent = store.store;
+  parent.appendChild(headingElement);
 
-
-
-
-
-
-
-
-
+  for (let i = 0; i < store.salesPerHour.length; i++) {
+    let liElement = document.createElement("li");
+    liElement.textContent = hours[i] + " " + store.salesPerHour[i];
+    parent.appendChild(liElement);
+  }
+}
